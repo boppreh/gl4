7c67ea55ebc7e2ace660ab2636dc752bb0f31115
BoppreH <boppreh@gmail.com>
2013-12-08 21:15:03 -0200

    Add `angle` to object position and inertia

--- gl4.js
+++ gl4.js
@@ -18,9 +18,13 @@ var gl4 = (function() {
 
         context.clearRect(0, 0, canvas.width, canvas.height);
         objects.forEach(function(object) {
-            object.pos.x += object.inertia.x;
-            object.pos.y += object.inertia.y;
-            context.drawImage(object.img, object.pos.x, object.pos.y);
+            object.move(object.inertia);
+
+            context.save();
+            context.translate(object.pos.x, object.pos.y);
+            context.rotate(object.pos.angle);
+            context.drawImage(object.img, -object.size.width / 2, -object.size.height / 2);
+            context.restore();
         });
 
         behaviors.forEach(function(behavior) {
@@ -48,13 +52,32 @@ var gl4 = (function() {
         },
 
         create: function(imageSource, objTags, pos, inertia) {
-            pos = pos || {x: 0, y: 0};
-            inertia = inertia || {x: 0, y: 0};
+            pos = pos || {x: 0, y: 0, angle: 0};
+            inertia = inertia || {x: 0, y: 0, angle: 0};
 
             var img = new Image();
             // TODO: make sure the image is already loaded before rendering.
             img.src = imageSource;
-            var object = {img: img, tags: tags, pos: pos, inertia: inertia};
+            var size = {width: img.width, height: img.height}
+
+            var object = {img: img,
+                          tags: tags,
+                          pos: pos,
+                          inertia: inertia,
+                          size: size,
+            
+                          move: function(speed) {
+                              this.pos.x += speed.x || 0;
+                              this.pos.y += speed.y || 0;
+                              this.pos.angle += speed.angle || 0;
+                          },
+            
+                          push: function(acceleration) {
+                              this.inertia.x += acceleration.x || 0;
+                              this.inertia.y += acceleration.y || 0;
+                              this.inertia.angle += acceleration.angle || 0;
+                          }};
+
             objects.push(object);
             objTags.forEach(function(tag) {
                 if (tags[tag] === undefined) {
@@ -77,7 +100,7 @@ var gl4 = (function() {
 })();
 
 var Mouse = (function() {
-    var obj = {pos: {x: 0, y: 0}, inertia: {x: 0, y: 0}};
+    var obj = {pos: {x: 0, y: 0, angle: 0}, inertia: {x: 0, y: 0, angle: 0}};
     window.onmousemove = function(event) {
         obj.inertia.x = event.clientX - obj.pos.x;
         obj.inertia.y = event.clientY - obj.pos.y;
@@ -90,19 +113,17 @@ var Mouse = (function() {
 
 function move(target, speed) {
     gl4.register(target, function(object) {
-        object.inertia.x = speed.x;
-        object.inertia.y = speed.y;
+        object.move(speed);
     });
 }
 
 function push(target, acceleration) {
     gl4.register(target, function(object) {
-        object.inertia.x += acceleration.x;
-        object.inertia.y += acceleration.y;
+        object.push(acceleration);
     });
 }
 
-gl4.create('star.png', ['star']);
+gl4.create('star.png', ['star'], {x: 100, y: 100, angle: Math.PI}, null);
 gl4.start();
 
 canvas.onclick = function() {
@@ -113,5 +134,5 @@ canvas.onclick = function() {
     }
 }
 
-push('star', {x: 0.1, y: 0.01})
+push('star', {angle: 0.001})
 
