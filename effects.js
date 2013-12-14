85400a9e4fc96804027409d1e1b8f1aa63f20dfa
BoppreH <boppreh@gmail.com>
2013-12-14 11:38:58 -0200

    Add regular box shadow effect

--- effects.js
+++ effects.js
@@ -6,3 +6,16 @@ function glow(color, size) {
         context.shadowBlur = size;
     } 
 }
+
+function shadow(offsetX, offsetY, blur, color) {
+    color = color === undefined ? 'black' : color;
+    blur = blur === undefined ? 3 : blur;
+    offsetX = offsetX === undefined ? 1 : offsetX;
+    offsetY = offsetY === undefined ? 1 : offsetY;
+    return function (context) {
+        context.shadowColor = color;
+        context.shadowOffsetX = offsetX;
+        context.shadowOffsetY = offsetY;
+        context.shadowBlur = blur;
+    }
+}
